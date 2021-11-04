91bacd54453f03b2617c845a005aedddd62fe66c
Connor Roberts <connoobets@gmail.com>
2021-11-04 15:21:18 -0600

    testing breedFetcher function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,20 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  // console.log(typeof body);
-  // console.log(body);
-  if (error) {
-    console.log(error);
-  }
-  const data = JSON.parse(body);
-  console.log(data);
-  // console.log(typeof data);
-  // console.log(data[0].weight);
-});
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (!error) {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        return callback('Breed not found', null);
+      }
+      return callback(null, data[0].description)
+    }
+    return callback(error, null);
+    
+  });
+};
+
+module.exports = {
+  fetchBreedDescription,
+};
---
+++ index.js
@@ -0,0 +1,11 @@
+const fetchBreedDescription = require('./breedFetcher');
+const args = process.argv.slice(2);
+const breedName = args[0];
+
+fetchBreedDescription(breedName, (err, desc) => {
+  if (err) {
+    console.log('Error fetch details:', err);
+  } else {
+    console.log(desc);
+  }
+});
